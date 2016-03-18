6ed8da1ab26c5a101f7c74304ffac884d57fab16
Jimmy Cook <jimmy.cook.1993@gmail.com>
2016-03-18 12:06:55 +0000

    switching to PV

---
+++ app/Views/templates/pages/listing.php
@@ -0,0 +1,24 @@
+<?php include(__DIR__ . '/../partials/header.php') ?>
+<?php include(__DIR__ . '/../partials/navbar.php') ?>
+
+<div class="container">
+    <div class="panel">
+
+        <div class="panel-heading">
+            <h2>$listing['name']</h2>
+        </div>
+
+        <div class="panel-body">
+
+        </div>
+
+    </div>
+</div>
+<pre>
+    <?php print_r($listing); ?>
+</pre>
+
+
+
+
+<?php include(__DIR__ . '/../partials/footer.php') ?>
