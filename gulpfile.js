eccf3053a915c0c4ba546ca5a3cfb8394a6bf7bd
Jimmy Cook <jimmy.cook.1993@gmail.com>
2016-04-06 18:24:03 +0100

    Fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 var elixir = require('laravel-elixir');
 
-// Pull in the vueify plugin for elixirs
-require('laravel-elixir-vueify');
-
 elixir(function(mix) {
     mix.less('app.less');
     mix.browserify('main.js');
